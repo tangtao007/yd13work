32978d3477353a75a765fcefb0a37dae80b5446b
tangtao <tangtao@tangtaodeMacBook-Pro.local>
2019-01-28 21:27:55 +0800

    delete

--- 01/work/bookApplication/backend/models/Book.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace backend\models;
-use yii\db\ActiveRecord;
-class Book extends ActiveRecord
-{
-    
-}
--- 01/work/bookApplication/frontend/views/book/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-use yii\helpers\Html;
-use yii\widgets\LinkPager;
-?>
-<h1>Countries</h1>
-<ul>
-2131313
-</ul>
